f31ae9bc7ba88267f1f68684d83fa09df3faf0de
shekofaamiri <shekofaamiri1@gmail.com>
2025-08-09 10:11:39 +0330

    constructor of point class

--- main2.cpp
+++ main2.cpp
@@ -16,3 +16,33 @@ public:
     double get_y();
     void print();
 };
+point::point()
+{
+    x = 0;
+    y = 0;
+}
+point::point(double x1, double y1)
+{
+    x = x1;
+    y = y1;
+}
+void point::set_x(double x1)
+{
+    x = x1;
+}
+void point::set_y(double y1)
+{
+    y = y1;
+}
+double point::get_x()
+{
+    return x;
+}
+double point::get_y()
+{
+    return y;
+}
+void point::print()
+{
+    cout << "point" << x << "," << y << endl;
+}
